bb95d8ed861c0b8dcb88deeb979f48d49781b765
Paul Ferrand <paulfd@outlook.fr>
2019-07-30 18:41:42 +0200

    Updated benchmarks

--- benchmarks/AudioBuffers.cpp
+++ benchmarks/AudioBuffers.cpp
@@ -6,8 +6,7 @@ static void AB_Joint_Fill_float(benchmark::State& state) {
   AudioBuffer<float> buffer (65535);
   float fillValue = 0.0f;
   for (auto _ : state) {
-    std::fill(buffer.begin(0), buffer.end(0), fillValue);
-    std::fill(buffer.begin(1), buffer.end(1), fillValue);
+    buffer.fill(fillValue);
     fillValue += 1.0f;
   }
 }
@@ -18,8 +17,7 @@ static void AB_Split_Fill_float(benchmark::State& state) {
   SplitAudioBuffer<float> buffer (65535);
   float fillValue = 0.0f;
   for (auto _ : state) {
-    std::fill(buffer.begin(0), buffer.end(0), fillValue);
-    std::fill(buffer.begin(1), buffer.end(1), fillValue);
+    buffer.fill(fillValue);
     fillValue += 1.0f;
   }
 }
@@ -29,8 +27,7 @@ static void AB_Joint_Fill_double(benchmark::State& state) {
   AudioBuffer<double> buffer (65535);
   double fillValue = 0.0;
   for (auto _ : state) {
-    std::fill(buffer.begin(0), buffer.end(0), fillValue);
-    std::fill(buffer.begin(1), buffer.end(1), fillValue);
+    buffer.fill(fillValue);
     fillValue += 1.0;
   }
 }
@@ -41,8 +38,7 @@ static void AB_Split_Fill_double(benchmark::State& state) {
   SplitAudioBuffer<double> buffer (65535);
   double fillValue = 0.0;
   for (auto _ : state) {
-    std::fill(buffer.begin(0), buffer.end(0), fillValue);
-    std::fill(buffer.begin(1), buffer.end(1), fillValue);
+    buffer.fill(fillValue);
     fillValue += 1.0;
   }
 }
