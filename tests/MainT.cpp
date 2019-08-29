a1913493babba3b074f4c7cff887fda4a9c610aa
paulfd <paulfd@outlook.fr>
2019-08-30 00:49:58 +0200

    Added licenses

--- tests/MainT.cpp
+++
@@ -1,8 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "catch2/catch.hpp"
-
-//==============================================================================
-// int main (int argc, char* argv[])
-// {
-//   return 0;
-// }
