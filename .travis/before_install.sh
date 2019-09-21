a838e0404bca664b1dbfc47c4ca3ab80b4f571e8
redtide <redtid3@gmail.com>
2019-09-21 15:38:14 +0200

    Fix Travis OSX build.

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -9,4 +9,5 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
   brew update
   brew upgrade cmake
+  brew install libsndfile
 fi
--- .travis/install.sh
+++ .travis/install.sh
@@ -4,6 +4,4 @@ set -e
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
   sudo apt-get install libasound2-dev libjack-jackd2-dev libsndfile1-dev
-elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-  brew install libsndfile
 fi
