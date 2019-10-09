5497476e653cf9d06be0178660cb5b8e6352465b
redtide <redtid3@gmail.com>
2019-10-09 23:31:06 +0200

    Updated doxygen files.

--- doxy/search/namespaces_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['config_437',['config',['../namespacesfz_1_1config.html',1,'sfz']]],
-  ['default_438',['Default',['../namespacesfz_1_1_default.html',1,'sfz']]],
-  ['regexes_439',['Regexes',['../namespacesfz_1_1_regexes.html',1,'sfz']]],
-  ['sfz_440',['sfz',['../namespacesfz.html',1,'']]],
-  ['simdconfig_441',['SIMDConfig',['../namespacesfz_1_1_s_i_m_d_config.html',1,'sfz']]]
-];
