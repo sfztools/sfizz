006e5b33b5bc8e3cbfe38cf97283e94166d90f82
Paul Ferrand <paul@ferrand.cc>
2019-12-18 10:24:40 +0100

    Removed the regexes

--- tests/ParsingT.cpp
+++ tests/ParsingT.cpp
@@ -21,7 +21,6 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "Regexes.h"
 #include "SfzHelpers.h"
 #include <iostream>
 #include "catch2/catch.hpp"
