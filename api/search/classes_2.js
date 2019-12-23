c2e8dd12be9833876998b438fbb04fbde20fe0c5
Travis CI <travis@Traviss-Mac-6.local>
2019-12-23 20:50:36 +0000

    Travis build: 111

--- api/search/classes_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['ccmap',['CCMap',['../classsfz_1_1_c_c_map.html',1,'sfz']]],
-  ['ccmap_3c_20sfz_3a_3arange_3c_20uint8_5ft_20_3e_20_3e',['CCMap&lt; sfz::Range&lt; uint8_t &gt; &gt;',['../classsfz_1_1_c_c_map.html',1,'sfz']]]
-];
--- api/search/classes_6.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['linearenvelope',['LinearEnvelope',['../classsfz_1_1_linear_envelope.html',1,'sfz']]],
-  ['linearenvelope_3c_20float_20_3e',['LinearEnvelope&lt; float &gt;',['../classsfz_1_1_linear_envelope.html',1,'sfz']]]
-];
