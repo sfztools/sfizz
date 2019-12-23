db2f64d3cc43b340771a97860131a2139fd2cdaa
redtide <redtid3@gmail.com>
2019-12-23 15:01:03 +0100

    Go back using gcc 7

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,7 +4,5 @@ set -ex
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
   sudo apt-get install libasound2-dev libjack-jackd2-dev libsndfile1-dev lv2-dev
-
-  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 100
   gcc -v && g++ -v && cmake --version && /usr/local/bin/cmake --version && $SHELL --version
 fi
