d06f97af0749f91d6dbac08f1bbffbbc6c33e10f
Travis CI User <travis@example.org>
2019-12-23 14:03:44 +0000

    Travis build: 108

---
+++ api/search/classes_9.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['range',['Range',['../classsfz_1_1_range.html',1,'sfz']]],
+  ['range_3c_20float_20_3e',['Range&lt; float &gt;',['../classsfz_1_1_range.html',1,'sfz']]],
+  ['range_3c_20int_20_3e',['Range&lt; int &gt;',['../classsfz_1_1_range.html',1,'sfz']]],
+  ['range_3c_20uint32_5ft_20_3e',['Range&lt; uint32_t &gt;',['../classsfz_1_1_range.html',1,'sfz']]],
+  ['range_3c_20uint8_5ft_20_3e',['Range&lt; uint8_t &gt;',['../classsfz_1_1_range.html',1,'sfz']]],
+  ['region',['Region',['../structsfz_1_1_region.html',1,'sfz']]]
+];
---
+++ api/search/functions_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hash',['hash',['../_string_view_helpers_8h.html#a0d70224bb798d5b61584ca93597224a6',1,'StringViewHelpers.h']]]
+];
