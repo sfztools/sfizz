648adf481fb1da9fbc9a2e4741d9ab8f24c550e1
Travis CI User <travis@example.org>
2019-12-23 17:35:23 +0000

    Travis build: 109

---
+++ api/search/classes_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['linearenvelope',['LinearEnvelope',['../classsfz_1_1_linear_envelope.html',1,'sfz']]],
+  ['linearenvelope_3c_20float_20_3e',['LinearEnvelope&lt; float &gt;',['../classsfz_1_1_linear_envelope.html',1,'sfz']]]
+];
---
+++ api/search/files_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['sfizz_2eh',['sfizz.h',['../sfizz_8h.html',1,'']]],
+  ['simdhelpers_2eh',['SIMDHelpers.h',['../_s_i_m_d_helpers_8h.html',1,'']]],
+  ['stringviewhelpers_2eh',['StringViewHelpers.h',['../_string_view_helpers_8h.html',1,'']]]
+];
