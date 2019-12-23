1abcdd835366342e72989ac453db0106c401fd9b
Travis CI <travis@Traviss-Mac-6.local>
2019-12-23 13:36:27 +0000

    Travis build: 106

--- api/search/all_11.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['velocitycurve',['velocityCurve',['../structsfz_1_1_region.html#a5df77f0225e44a9c9c0f42f6c35188c7',1,'sfz::Region']]],
-  ['voice',['Voice',['../classsfz_1_1_voice.html',1,'sfz::Voice'],['../classsfz_1_1_voice.html#a82d3899e05531ff6ce843a8f684286a8',1,'sfz::Voice::Voice()']]]
-];
--- api/search/classes_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['historicalbuffer',['HistoricalBuffer',['../classsfz_1_1_historical_buffer.html',1,'sfz']]],
-  ['historicalbuffer_3c_20float_20_3e',['HistoricalBuffer&lt; float &gt;',['../classsfz_1_1_historical_buffer.html',1,'sfz']]]
-];
