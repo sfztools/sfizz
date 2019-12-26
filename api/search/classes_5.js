a651c43fea73659f95e47dd19e4511d9db74800c
Travis CI User <travis@example.org>
2019-12-26 22:43:57 +0000

    Travis build: 117

---
+++ api/search/classes_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['historicalbuffer',['HistoricalBuffer',['../classsfz_1_1_historical_buffer.html',1,'sfz']]],
+  ['historicalbuffer_3c_20float_20_3e',['HistoricalBuffer&lt; float &gt;',['../classsfz_1_1_historical_buffer.html',1,'sfz']]]
+];
