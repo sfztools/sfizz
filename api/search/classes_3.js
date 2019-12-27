12b42cd23afe2780bff60f347cec1c15e76ba81b
Travis CI User <travis@example.org>
2019-12-27 00:51:31 +0000

    Travis build: 118

---
+++ api/search/classes_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['egdescription',['EGDescription',['../structsfz_1_1_e_g_description.html',1,'sfz']]],
+  ['eventenvelope',['EventEnvelope',['../classsfz_1_1_event_envelope.html',1,'sfz']]],
+  ['eventenvelope_3c_20float_20_3e',['EventEnvelope&lt; float &gt;',['../classsfz_1_1_event_envelope.html',1,'sfz']]]
+];
