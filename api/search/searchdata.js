b51957a45d81c4fbd8fbc49ca90bb2845f9ec388
Travis CI User <travis@example.org>
2019-12-27 11:24:51 +0000

    Travis build: 120

---
+++ api/search/searchdata.js
@@ -0,0 +1,33 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghilmnoprstvw~",
+  1: "abcefhlmorsv",
+  2: "rs",
+  3: "ms",
+  4: "abcdefghilmnoprstvw~",
+  5: "s",
+  6: "gs"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables",
+  6: "Pages"
+};
+
