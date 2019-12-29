f5169a249db405362409658c2f872f11361420a9
Travis CI User <travis@example.org>
2019-12-29 23:13:20 +0000

    Travis build: 128

--- api/search/all_b.js
+++ api/search/all_b.js
@@ -6,6 +6,7 @@ var searchData=
   ['normalizepercents',['normalizePercents',['../namespacesfz.html#ac1f49ce61c8d1db977f17efc4ee199bf',1,'sfz']]],
   ['normalizevelocity',['normalizeVelocity',['../namespacesfz.html#a6fd3f3f2641b8065321f00b8e2e3cbec',1,'sfz']]],
   ['noteoff',['noteOff',['../classsfz_1_1_synth.html#a522c431f0ff565a69ab88925b7b89af3',1,'sfz::Synth']]],
+  ['noteoffevent',['noteOffEvent',['../classsfz_1_1_midi_state.html#af8e3a3f7d094d2137b4a6d3c47a70448',1,'sfz::MidiState']]],
   ['noteon',['noteOn',['../classsfz_1_1_synth.html#acb204cbc70f439d8fe5260f4b910157b',1,'sfz::Synth']]],
   ['noteonevent',['noteOnEvent',['../classsfz_1_1_midi_state.html#a06971a2b813884edfe8ffb6c39e4d15c',1,'sfz::MidiState']]]
 ];
