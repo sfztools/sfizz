c6b188abf9bf83ec787b487e09f3d34071cad5d3
Paul Ferrand <paul@ferrand.cc>
2020-01-04 00:59:47 +0100

    Change the case in the lv2 name

--- cmake/LV2Config.cmake
+++ cmake/LV2Config.cmake
@@ -2,7 +2,7 @@
 # TODO: generate version from git
 set (LV2PLUGIN_VERSION_MINOR   0)
 set (LV2PLUGIN_VERSION_MICRO   1)
-set (LV2PLUGIN_NAME            "Sfizz")
+set (LV2PLUGIN_NAME            "sfizz")
 set (LV2PLUGIN_COMMENT         "SFZ sampler")
 set (LV2PLUGIN_URI             "http://sfztools.github.io/sfizz")
 set (LV2PLUGIN_AUTHOR          "Paul Ferrand")
