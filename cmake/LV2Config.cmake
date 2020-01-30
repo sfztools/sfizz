7377bb62a842420c4a4b6c6769d29f199759bf8b
Paul Ferrand <paul@ferrand.cc>
2020-01-30 10:08:38 +0100

    Added a configure switch and license information for the distribution of the LV2 plugin

--- cmake/LV2Config.cmake
+++ cmake/LV2Config.cmake
@@ -5,9 +5,15 @@ set (LV2PLUGIN_VERSION_MICRO   0)
 set (LV2PLUGIN_NAME            "sfizz")
 set (LV2PLUGIN_COMMENT         "SFZ sampler")
 set (LV2PLUGIN_URI             "http://sfztools.github.io/sfizz")
+set (LV2PLUGIN_REPOSITORY      "https://github.com/sfztools/sfizz")
 set (LV2PLUGIN_AUTHOR          "Paul Ferrand")
 set (LV2PLUGIN_EMAIL           "paul at ferrand dot cc")
-set (LV2PLUGIN_SPDX_LICENSE_ID "ISC")
+if (SFIZZ_USE_VCPKG)
+    set (LV2PLUGIN_SPDX_LICENSE_ID "LGPL-3.0-only")
+else()
+    set (LV2PLUGIN_SPDX_LICENSE_ID "ISC")
+endif()
+
 
 if (WIN32)
     set (LV2PLUGIN_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/lv2" CACHE STRING
--- lv2/sfizz.c
+++ lv2/sfizz.c
@@ -18,6 +18,11 @@
   purpose with or without fee is hereby granted, provided that the above
   copyright notice and this permission notice appear in all copies.
 
+  Compiling this plugin statically against libsndfile implies distributing it
+  under the terms of the LGPL v3 license. See the LICENSE.md file for more
+  information. If you did not receive a LICENSE.md file, inform the current
+  maintainer.
+
   THIS SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
