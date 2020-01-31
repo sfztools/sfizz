8b7bba682cdc1ce2ae1e99377f3cc36d06ba58bf
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-01-31 23:24:22 +0100

    Compatibility with CMake 3.5

--- cmake/CheckIPO.cmake
+++ cmake/CheckIPO.cmake
@@ -1,9 +1,21 @@
 # Added in CMake 3.9
+
+if (CMAKE_VERSION VERSION_LESS 3.9)
+    message (WARNING "\nIPO checks are only available on CMake 3.9 and later.")
+
+    set(ENABLE_LTO OFF CACHE BOOL "" FORCE)
+
+    function (SFIZZ_ENABLE_LTO_IF_NEEDED TARGET)
+    endfunction()
+
+    return()
+endif()
+
 include (CheckIPOSupported)
 check_ipo_supported (RESULT result OUTPUT output)
 
 if (CMAKE_SYSTEM_PROCESSOR STREQUAL armv7l)
-    set(ENABLE_LTO OFF)
+    set(ENABLE_LTO OFF CACHE BOOL "" FORCE)
 endif()
 
 if (result AND ENABLE_LTO AND CMAKE_BUILD_TYPE STREQUAL "Release")
@@ -14,7 +26,7 @@ else()
     else()
         message (WARNING "\nIPO was disabled or not in a Release build.")
     endif()
-    set (ENABLE_LTO OFF)
+    set(ENABLE_LTO OFF CACHE BOOL "" FORCE)
 endif()
 
 function (SFIZZ_ENABLE_LTO_IF_NEEDED TARGET)
