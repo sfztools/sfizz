3c408ce2102b9682fe7072ba75b3c6edcbcc5432
Travis CI User <travis@example.org>
2020-02-06 23:47:22 +0000

    Travis build: 263

--- api/search/all_d.js
+++ api/search/all_d.js
@@ -6,7 +6,7 @@ var searchData=
   ['pitchwheel',['pitchWheel',['../classsfz_1_1_synth.html#af082ebe877007a2206134161abe9be7c',1,'sfz::Synth']]],
   ['pow2db',['pow2db',['../_math_helpers_8h.html#af01e41fe4cf56c50b1782c28b923446e',1,'MathHelpers.h']]],
   ['preloadfile',['preloadFile',['../classsfz_1_1_file_pool.html#a3430758f4b1ab97c27620de5fc727155',1,'sfz::FilePool']]],
-  ['process',['process',['../classsfz_1_1_filter.html#a205c453cd9bc613e0fe7cbde2674cd3c',1,'sfz::Filter']]],
-  ['processmodulated',['processModulated',['../classsfz_1_1_filter.html#ad5f464b9f021b64cfa9f0a00ac0fe6e5',1,'sfz::Filter']]],
+  ['process',['process',['../classsfz_1_1_filter.html#a205c453cd9bc613e0fe7cbde2674cd3c',1,'sfz::Filter::process()'],['../classsfz_1_1_filter_eq.html#aa5065a3bd6dd49841a07db1283a6b08b',1,'sfz::FilterEq::process()']]],
+  ['processmodulated',['processModulated',['../classsfz_1_1_filter.html#ad5f464b9f021b64cfa9f0a00ac0fe6e5',1,'sfz::Filter::processModulated()'],['../classsfz_1_1_filter_eq.html#aba368ce0bdec4862ce219a53da780a98',1,'sfz::FilterEq::processModulated()']]],
   ['push',['push',['../classsfz_1_1_historical_buffer.html#ac2d93a98435fa5be487bb299990ab26b',1,'sfz::HistoricalBuffer']]]
 ];
--- api/search/classes_4.js
+++ api/search/classes_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['filepool',['FilePool',['../classsfz_1_1_file_pool.html',1,'sfz']]],
-  ['filter',['Filter',['../classsfz_1_1_filter.html',1,'sfz']]]
+  ['filter',['Filter',['../classsfz_1_1_filter.html',1,'sfz']]],
+  ['filtereq',['FilterEq',['../classsfz_1_1_filter_eq.html',1,'sfz']]]
 ];
