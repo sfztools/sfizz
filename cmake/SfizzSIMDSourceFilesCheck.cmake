afbe8c7e48c4d5d6fcf870a265d2bc61ff1061a1
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-02-09 14:58:21 +0100

    Detect SIMD availability from compiler

--- cmake/SfizzSIMDSourceFilesCheck.cmake
+++ cmake/SfizzSIMDSourceFilesCheck.cmake
@@ -1,31 +1,14 @@
-# Check SIMD
-include (CheckIncludeFiles)
-CHECK_INCLUDE_FILES(x86intrin.h HAVE_X86INTRIN_H)
-CHECK_INCLUDE_FILES(intrin.h HAVE_INTRIN_H)
-
-if (!APPLE)
-CHECK_INCLUDE_FILES (arm_neon.h HAVE_ARM_NEON_H)
-endif()
+set (SFIZZ_SIMD_SOURCES
+    sfizz/SIMDSSE.cpp
+    sfizz/SIMDNEON.cpp
+    sfizz/SIMDDummy.cpp)
 
 # SIMD checks
-if (HAVE_X86INTRIN_H AND UNIX)
-    add_compile_options (-DHAVE_X86INTRIN_H)
-    set (SFIZZ_SIMD_SOURCES sfizz/SIMDSSE.cpp)
-elseif (HAVE_INTRIN_H AND WIN32)
-    if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-        add_compile_options ("/DHAVE_INTRIN_H")
-    else()
-        add_compile_options ("-DHAVE_INTRIN_H")
-    endif()
-    set (SFIZZ_SIMD_SOURCES sfizz/SIMDSSE.cpp)
-elseif (CMAKE_SYSTEM_PROCESSOR STREQUAL "armv7l")
+if (CMAKE_SYSTEM_PROCESSOR STREQUAL "armv7l")
     add_compile_options (-DHAVE_ARM_NEON_H)
     add_compile_options (-mfpu=neon)
     add_compile_options (-march=native)
     add_compile_options (-mtune=cortex-a53)
-    set (SFIZZ_SIMD_SOURCES sfizz/SIMDNEON.cpp)
-else()
-    set (SFIZZ_SIMD_SOURCES sfizz/SIMDDummy.cpp)
 endif()
 
 set (SFIZZ_SOURCES ${SFIZZ_SOURCES} ${SFIZZ_SIMD_SOURCES})
