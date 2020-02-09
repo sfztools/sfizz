52469f1d33390182ba00c8127079b6e9296da595
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-02-09 19:02:14 +0100

    Removed hardcoded build flags for ARM

--- cmake/SfizzSIMDSourceFilesCheck.cmake
+++ cmake/SfizzSIMDSourceFilesCheck.cmake
@@ -3,12 +3,4 @@ set (SFIZZ_SIMD_SOURCES
     sfizz/SIMDNEON.cpp
     sfizz/SIMDDummy.cpp)
 
-# SIMD checks
-if (CMAKE_SYSTEM_PROCESSOR STREQUAL "armv7l")
-    add_compile_options (-DHAVE_ARM_NEON_H)
-    add_compile_options (-mfpu=neon)
-    add_compile_options (-march=native)
-    add_compile_options (-mtune=cortex-a53)
-endif()
-
-set (SFIZZ_SOURCES ${SFIZZ_SOURCES} ${SFIZZ_SIMD_SOURCES})
+list (APPEND SFIZZ_SOURCES ${SFIZZ_SIMD_SOURCES})
