1ee2424f2d374be9d8856243fd36c45d5aa29b33
Travis CI User <travis@example.org>
2020-02-11 07:18:24 +0000

    Travis build: 305

--- api/search/all_b.js
+++ api/search/all_b.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['normalizebend',['normalizeBend',['../namespacesfz.html#a082ee2dc6715392c07a5f6a5216c537b',1,'sfz']]],
   ['normalizecc',['normalizeCC',['../namespacesfz.html#a9b65015c18e6461f96cb9af4e30cdee8',1,'sfz']]],
-  ['normalizenegativepercents',['normalizeNegativePercents',['../namespacesfz.html#abfcbb9f0d7899c187863d5e457a84fca',1,'sfz']]],
   ['normalizepercents',['normalizePercents',['../namespacesfz.html#ac1f49ce61c8d1db977f17efc4ee199bf',1,'sfz']]],
   ['normalizevelocity',['normalizeVelocity',['../namespacesfz.html#a6fd3f3f2641b8065321f00b8e2e3cbec',1,'sfz']]],
   ['noteoff',['noteOff',['../classsfz_1_1_synth.html#a522c431f0ff565a69ab88925b7b89af3',1,'sfz::Synth']]],
