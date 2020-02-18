a59734f0003abe64f0fcd2db2f16bac3ebfe8ae9
redtide <redtid3@gmail.com>
2020-02-18 01:47:13 +0100

    Update doxigen travis script [ci skip]

--- .travis/update_dox.sh
+++ .travis/update_dox.sh
@@ -12,9 +12,8 @@ doxygen Doxyfile
 git fetch --depth=1 https://github.com/${TRAVIS_REPO_SLUG}.git refs/heads/gh-pages:refs/remotes/origin/gh-pages
 git checkout origin/gh-pages
 git checkout -b gh-pages
-rm -rf ./api
-mv _api api
-git add api && git commit -m "Travis build: ${TRAVIS_BUILD_NUMBER}"
+mv _api api/${TRAVIS_TAG}
+git add api/${TRAVIS_TAG} && git commit -m "Release ${TRAVIS_TAG} (Travis build: ${TRAVIS_BUILD_NUMBER})"
 git remote add origin-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
 git push --quiet --set-upstream origin-pages gh-pages
 git checkout ${TRAVIS_BRANCH}
