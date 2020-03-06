99317e764d92ae3bb002e8995e160cd5edc6ba1e
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-03-06 19:55:36 +0100

    Update the installer script for VST

--- vst/cmake/Vst3.cmake
+++ vst/cmake/Vst3.cmake
@@ -255,22 +255,3 @@ function(plugin_add_vstgui NAME)
     target_include_directories("${NAME}" PRIVATE
         external/steinberg/src)
 endfunction()
-
-# --- VST3 Bundle architecture ---
-if(NOT VST3_PACKAGE_ARCHITECTURE)
-    if(APPLE)
-        # VST3 packages are universal on Apple, architecture string not needed
-    else()
-        if(CMAKE_SYSTEM_PROCESSOR MATCHES "^(x86_64|amd64|AMD64)$")
-            set(VST3_PACKAGE_ARCHITECTURE "x86_64")
-        elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "^(i.86|x86)$")
-            if(WIN32)
-                set(VST3_PACKAGE_ARCHITECTURE "x86")
-            else()
-                set(VST3_PACKAGE_ARCHITECTURE "i386")
-            endif()
-        else()
-            message(FATAL_ERROR "We don't know this architecture for VST3: ${CMAKE_SYSTEM_PROCESSOR}.")
-        endif()
-    endif()
-endif()
