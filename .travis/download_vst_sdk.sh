88593a9a9f4c3e03741d6f9db9f81dbfe80a408a
Paul Fd <paul@ferrand.cc>
2020-03-14 14:05:05 +0100

    Check if the sdk is downloaded already

--- .travis/download_vst_sdk.sh
+++ .travis/download_vst_sdk.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ex
 
+vst_download_prefix="vst/download"
 vst_sdk_archive="vst-sdk_3.6.14_build-24_2019-11-29.zip"
-wget "https://download.steinberg.net/sdk_downloads/${vst_sdk_archive}"
+mkdir -p ${vst_download_prefix}
+if ! [[ -f "${vst_download_prefix}/${vst_sdk_archive}" ]]; then
+  wget -P ${vst_download_prefix} "https://download.steinberg.net/sdk_downloads/${vst_sdk_archive}"
+fi
 mkdir -p vst/external
-unzip -ouq ${vst_sdk_archive} -d vst/external
+unzip -ouq "${vst_download_prefix}/${vst_sdk_archive}" -d "vst/external"
