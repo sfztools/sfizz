36cb6db97495ebad446cd20ebf887ebd151f2f83
Paul Fd <paul@ferrand.cc>
2020-03-26 23:14:33 +0100

    Remove the export definitions from the macros

--- src/sfizz/Macros.h
+++ src/sfizz/Macros.h
@@ -8,16 +8,6 @@
 
 #define UNUSED(x) (void)(x)
 
-#if defined SFIZZ_EXPORT_SYMBOLS
-  #if defined _WIN32
-    #define SFIZZ_EXPORTED_API __declspec(dllexport)
-  #else
-    #define SFIZZ_EXPORTED_API __attribute__ ((visibility ("default")))
-  #endif
-#else
-  #define SFIZZ_EXPORTED_API
-#endif
-
 #if __cplusplus > 201103L
 #define CXX14_CONSTEXPR constexpr
 #define CXX11_MOVE(x) x
