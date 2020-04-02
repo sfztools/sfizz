3ed8c4f7719899217dddf9e2ce70d4943a66e499
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-04-02 16:30:37 +0200

    Print the system processor in summary

--- cmake/SfizzConfig.cmake
+++ cmake/SfizzConfig.cmake
@@ -86,6 +86,7 @@ function (show_build_info_if_needed)
         message (STATUS "
 Project name:                  ${PROJECT_NAME}
 Build type:                    ${CMAKE_BUILD_TYPE}
+Build processor:               ${SFIZZ_SYSTEM_PROCESSOR}
 Build using LTO:               ${ENABLE_LTO}
 Build as shared library:       ${SFIZZ_SHARED}
 Build JACK stand-alone client: ${SFIZZ_JACK}
