4e7f15328ac221d918977aa3a8f767d032d0b0f5
redtide <redtid3@gmail.com>
2020-04-06 14:19:55 +0200

    Auto update the api index page

--- .travis/update_dox.sh
+++ .travis/update_dox.sh
@@ -5,11 +5,13 @@ set -x # No fail, we need to go back to the original branch at the end
 
 mkdir build && cd build && cmake -DSFIZZ_JACK=OFF -DSFIZZ_SHARED=OFF -DSFIZZ_LV2=OFF .. && cd ..
 doxygen Doxyfile
+./doxygen/scripts/generate_api_index.sh
 git fetch --depth=1 https://github.com/${TRAVIS_REPO_SLUG}.git refs/heads/gh-pages:refs/remotes/origin/gh-pages
 git checkout origin/gh-pages
 git checkout -b gh-pages
 mv _api api/${TRAVIS_TAG}
-git add api/${TRAVIS_TAG} && git commit -m "Release ${TRAVIS_TAG} (Travis build: ${TRAVIS_BUILD_NUMBER})"
+mv api_index.md api/index.md
+git add api && git commit -m "Release ${TRAVIS_TAG} (Travis build: ${TRAVIS_BUILD_NUMBER})"
 git remote add origin-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
 git push --quiet --set-upstream origin-pages gh-pages
 git checkout ${TRAVIS_BRANCH}
--- doxygen/scripts/generate_api_index.sh
+++ doxygen/scripts/generate_api_index.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # Must be called from the root directory
-cat >>index.md <<EOF
+if [[ -f api_index.md ]]; then rm api_index.md; fi
+cat >>api_index.md <<EOF
 ---
 title: "API"
 ---
 EOF
+tags=()
 for tag in $(git tag --list); do
   if [[ ${tag} != "list" && ${tag} != *"test"* ]]; then
-    echo "- [${tag}](${tag})" >> index.md
+    tag=$(echo "${tag}" | sed -r 's/v//g')
+    tags+=("${tag}")
   fi
+done
+IFS=$'\n' tags=($(sort <<<"${tags[*]}")); unset IFS
+for tag in "${tags[@]}"; do
+  echo "- [${tag}](${tag})" >> api_index.md
 done
