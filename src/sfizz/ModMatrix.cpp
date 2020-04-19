5997ac0a0841a2444b40c594e0d89dae71e2eb66
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-04-19 10:53:40 +0200

    Add the modulation matrix

---
+++ src/sfizz/ModMatrix.cpp
@@ -0,0 +1,211 @@
+// SPDX-License-Identifier: BSD-2-Clause
+
+// This code is part of the sfizz library and is licensed under a BSD 2-clause
+// license. You should have receive a LICENSE.md file along with the code.
+// If not, contact the sfizz maintainers at https://github.com/sfztools/sfizz
+
+#include "ModMatrix.h"
+#include "Config.h"
+#include "StringViewHelpers.h"
+#include "SIMDHelpers.h"
+#include <algorithm>
+#include <cassert>
+
+size_t std::hash<sfz::ModKey>::operator()(const sfz::ModKey &key) const
+{
+    auto hashableInt32 = [](const int32_t *p) -> absl::string_view {
+        return absl::string_view(reinterpret_cast<const char *>(p), sizeof(*p));
+    };
+    uint64_t k = key.id;
+    k = ::hash(hashableInt32(&key.index1), k);
+    k = ::hash(hashableInt32(&key.index2), k);
+    return k;
+}
+
+bool sfz::ModKey::operator==(const ModKey &other) const noexcept
+{
+    return id == other.id && index1 == other.index1 && index2 == other.index2;
+}
+
+bool sfz::ModKey::operator!=(const ModKey &other) const noexcept
+{
+    return !this->operator==(other);
+}
+
+namespace sfz {
+
+ModMatrix::ModMatrix()
+    : _samplesPerBlock(config::defaultSamplesPerBlock)
+{
+    _temp.resize(_samplesPerBlock);
+}
+
+ModMatrix::~ModMatrix()
+{
+}
+
+void ModMatrix::clear()
+{
+    _sourceIndex.clear();
+    _targetIndex.clear();
+    _sources.clear();
+    _targets.clear();
+}
+
+void ModMatrix::setSamplesPerBlock(unsigned samplesPerBlock)
+{
+    _samplesPerBlock = samplesPerBlock;
+
+    for (Source &source : _sources)
+        source.buffer.resize(samplesPerBlock);
+    for (Target &target : _targets)
+        target.buffer.resize(samplesPerBlock);
+
+    _temp.resize(samplesPerBlock);
+}
+
+ModMatrix::SourceId ModMatrix::registerSource(ModKey key, ModGenerator* gen, bool genOwned, int32_t flags)
+{
+    assert(gen);
+
+    std::unique_ptr<ModGenerator> genOwnerPtr;
+    if (genOwned)
+        genOwnerPtr.reset(gen);
+
+    auto it = _sourceIndex.find(key);
+    if (it != _sourceIndex.end()) {
+        SourceId id;
+        id.index = it->second;
+        return id;
+    }
+
+    SourceId id;
+    id.index = _sources.size();
+    _sources.emplace_back();
+
+    Source &source = _sources.back();
+    source.gen = gen;
+    source.genOwnerPtr = std::move(genOwnerPtr);
+    source.flags = flags;
+    source.bufferReady = false;
+    source.buffer.resize(_samplesPerBlock);
+
+    _sourceIndex[key] = id.index;
+    return id;
+}
+
+ModMatrix::TargetId ModMatrix::registerTarget(ModKey key, int32_t flags)
+{
+    auto it = _targetIndex.find(key);
+    if (it != _targetIndex.end()) {
+        TargetId id;
+        id.index = it->second;
+        return id;
+    }
+
+    TargetId id;
+    id.index = _targets.size();
+    _targets.emplace_back();
+
+    Target &target = _targets.back();
+    target.flags = flags;
+    target.bufferReady = false;
+    target.buffer.resize(_samplesPerBlock);
+
+    _targetIndex[key] = id.index;
+    return id;
+}
+
+bool ModMatrix::connect(SourceId sourceId, TargetId targetId)
+{
+    uint32_t sourceIndex = sourceId.index;
+    uint32_t targetIndex = targetId.index;
+
+    if (sourceIndex >= _sources.size() || targetIndex >= _targets.size())
+        return false;
+
+    Target& target = _targets[targetIndex];
+    std::vector<uint32_t> &list = target.sources;
+
+    // add the source if not already present
+    if (std::find(list.begin(), list.end(), sourceIndex) == list.end())
+        list.push_back(sourceIndex);
+
+    return true;
+}
+
+void ModMatrix::beginCycle(unsigned numFrames)
+{
+    _numFrames = numFrames;
+
+    for (Source &source : _sources)
+        source.bufferReady = false;
+    for (Target &target : _targets)
+        target.bufferReady = false;
+}
+
+void ModMatrix::beginVoice(unsigned voiceNum)
+{
+    _voiceNum = voiceNum;
+
+    for (Source &source : _sources) {
+        if (source.flags & kModIsPerVoice)
+            source.bufferReady = false;
+    }
+    for (Target &target : _targets) {
+        if (target.flags & kModIsPerVoice)
+            target.bufferReady = false;
+    }
+}
+
+float* ModMatrix::getModulation(ModKey targetKey)
+{
+    auto it = _targetIndex.find(targetKey);
+    if (it == _targetIndex.end())
+        return nullptr;
+
+    uint32_t targetIndex = it->second;
+    Target &target = _targets[targetIndex];
+
+    absl::Span<float> buffer = absl::MakeSpan(target.buffer);
+
+    // check if already processed
+    if (target.bufferReady)
+        return buffer.data();
+
+    const uint32_t numFrames = _numFrames;
+
+    // set the ready flag to prevent a cycle
+    // in case there is, be sure to initialize the buffer
+    target.bufferReady = true;
+    sfz::fill(buffer, 0.0f);
+
+    const std::vector<uint32_t> &sourceIndices = target.sources;
+    size_t numSources = sourceIndices.size();
+
+    // generate the first source in buffer
+    if (numSources > 0) {
+        Source &source = _sources[sourceIndices[0]];
+        source.gen->process(_voiceNum, buffer.data(), numFrames);
+    }
+
+    // generate next sources in temporary buffer
+    // then add or multiply, depending on target flags
+    absl::Span<float> temp = absl::MakeSpan(_temp);
+    for (uint32_t s = 1; s < numSources; ++s) {
+        Source &source = _sources[sourceIndices[s]];
+        source.gen->process(_voiceNum, temp.data(), numFrames);
+        if (target.flags & kModIsMultiplicative) {
+            for (uint32_t i = 0; i < numFrames; ++i)
+                buffer[i] *= temp[i];
+        }
+        else {
+            for (uint32_t i = 0; i < numFrames; ++i)
+                buffer[i] += temp[i];
+        }
+    }
+
+    return buffer.data();
+}
+
+} // namespace sfz
---
+++ src/sfizz/ModMatrix.h
@@ -0,0 +1,181 @@
+// SPDX-License-Identifier: BSD-2-Clause
+
+// This code is part of the sfizz library and is licensed under a BSD 2-clause
+// license. You should have receive a LICENSE.md file along with the code.
+// If not, contact the sfizz maintainers at https://github.com/sfztools/sfizz
+
+#pragma once
+#include "Buffer.h"
+#include "absl/container/flat_hash_map.h"
+#include <vector>
+#include <memory>
+#include <cstdint>
+
+namespace sfz { class ModKey; }
+
+namespace std {
+    template <> struct hash<sfz::ModKey> {
+        size_t operator()(const sfz::ModKey &key) const;
+    };
+}
+
+namespace sfz {
+
+/**
+ * @brief Identifier of a modulation source or target
+ */
+class ModKey {
+public:
+    //! Identifier, a letter-only hash
+    uint64_t id = 0;
+    //! First index of the key or -1 (eg. `N` in `lfoN`)
+    int32_t index1 = -1;
+    //! Second index of the key or -1 (eg. `X` in `lfoN_eqX`)
+    int32_t index2 = -1;
+
+public:
+    bool operator==(const ModKey &other) const noexcept;
+    bool operator!=(const ModKey &other) const noexcept;
+};
+
+
+/**
+ * @brief Modulation bit flags (S=source, T=target, ST=either)
+ */
+enum ModFlags {
+    //! This modulation is global (the default). (ST)
+    kModIsPerCycle = 0,
+    //! This modulation is updated separately for every voice (ST)
+    kModIsPerVoice = 1 << 1,
+    //! This target is additive (the default). (T)
+    kModIsAdditive = 0,
+    //! This target is multiplicative (T)
+    kModIsMultiplicative = 1 << 2,
+};
+
+/**
+ * @brief Generator for modulation sources
+ */
+class ModGenerator {
+public:
+    virtual ~ModGenerator() {}
+
+    /**
+     * @brief Generate a cycle of the modulator
+     *
+     * @param voiceNum voice number if the generator is per-voice, otherwise undefined
+     * @param buffer output buffer
+     * @param numFrames number of frames to generate
+     */
+    virtual void process(int32_t voiceNum, float *buffer, unsigned numFrames) = 0;
+};
+
+/**
+ * @brief Modulation matrix
+ */
+class ModMatrix {
+public:
+    ModMatrix();
+    ~ModMatrix();
+
+    //! Identifier of a modulation source
+    struct SourceId { int32_t index = -1; };
+
+    //! Identifier of a modulation target
+    struct TargetId { int32_t index = -1; };
+
+    /**
+     * @brief Reset the matrix to the empty state.
+     */
+    void clear();
+
+    /**
+     * @brief Resize the modulation buffers.
+     *
+     * @param samplesPerBlock new block size
+     */
+    void setSamplesPerBlock(unsigned samplesPerBlock);
+
+    /**
+     * @brief Register a modulation source inside the matrix.
+     * If it is already present, it just returns the existing id.
+     *
+     * @param key source key
+     * @param gen generator
+     * @param genOwned whether the source takes ownership of the generator
+     * @param flags source flags
+     */
+    SourceId registerSource(ModKey key, ModGenerator* gen, bool genOwned, int32_t flags);
+
+    /**
+     * @brief Register a modulation target inside the matrix.
+     *
+     * @param key target key
+     * @param flags target flags
+     */
+    TargetId registerTarget(ModKey key, int32_t flags);
+
+    /**
+     * @brief Connect a source and a destination inside the matrix.
+     *
+     * @param sourceId
+     * @param targetId
+     * @return true if the connection was successfully made, otherwise false
+     */
+    bool connect(SourceId sourceId, TargetId targetId);
+
+    /**
+     * @brief Start modulation processing for the entire cycle.
+     * This clears all the buffers.
+     *
+     * @param numFrames
+     */
+    void beginCycle(unsigned numFrames);
+
+    /**
+     * @brief Start modulation processing for a given voice.
+     * This clears all the buffers which are per-voice.
+     *
+     * @param voiceNum the number of the current voice
+     */
+    void beginVoice(unsigned voiceNum);
+
+    /**
+     * @brief Get the modulation buffer for the given target.
+     * If the target does not exist, the result is null.
+     *
+     * @param targetKey key of the modulation target
+     */
+    float* getModulation(ModKey targetKey);
+
+private:
+    uint32_t _samplesPerBlock {};
+
+    uint32_t _numFrames {};
+    uint32_t _voiceNum {};
+
+    struct Source {
+        ModGenerator* gen {};
+        std::unique_ptr<ModGenerator> genOwnerPtr;
+        int32_t flags {};
+        bool bufferReady {};
+        Buffer<float> buffer;
+    };
+
+    struct Target {
+        int32_t flags {};
+        std::vector<uint32_t> sources;
+        bool bufferReady {};
+        Buffer<float> buffer;
+    };
+
+    absl::flat_hash_map<ModKey, uint32_t> _sourceIndex;
+    absl::flat_hash_map<ModKey, uint32_t> _targetIndex;
+
+    std::vector<Source> _sources;
+    std::vector<Target> _targets;
+
+    Buffer<float> _temp;
+};
+
+} // namespace sfz
