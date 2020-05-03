52b4f1b0a7df1fdd8b092e6abd4752fef01e4cbf
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-05-03 08:01:24 +0200

    Ensure to pass the SMF file path as 8-bit UTF

--- clients/sfizz_render.cpp
+++ clients/sfizz_render.cpp
@@ -143,7 +143,7 @@ int main(int argc, char** argv)
     ERROR_IF(!synth.loadSfzFile(sfzPath), "There was an error loading the SFZ file.");
     LOG_INFO(synth.getNumRegions() << " regions in the SFZ.");
 
-    fmidi_smf_u midiFile { fmidi_smf_file_read(midiPath.c_str()) };
+    fmidi_smf_u midiFile { fmidi_smf_file_read(midiPath.u8string().c_str()) };
     ERROR_IF(!midiFile, "Can't read " << midiPath);
 
     const auto* midiInfo = fmidi_smf_get_info(midiFile.get());
@@ -154,7 +154,6 @@ int main(int argc, char** argv)
     if (useEOT) {
         LOG_INFO("-- Cutting the rendering at the last MIDI End of Track message");
     }
-
 
     SndfileHandle outputFile (outputPath, SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_16, 2, sampleRate);
     ERROR_IF(outputFile.error() != 0, "Error writing out the wav file: " << outputFile.strError());
