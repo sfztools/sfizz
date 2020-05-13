af6dbea70c9a6dd5f6458940e7291251e4c4d69d
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-05-13 08:38:11 -0700

    Build and deploy AU in Travis

--- .travis/script_osx.sh
+++ .travis/script_osx.sh
@@ -4,10 +4,12 @@ set -ex
 mkdir -p build/${INSTALL_DIR} && cd build
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DSFIZZ_VST=ON \
+      -DSFIZZ_AU=ON \
       -DSFIZZ_TESTS=OFF \
       -DCMAKE_CXX_STANDARD=17 \
       -DLV2PLUGIN_INSTALL_DIR=/Library/Audio/Plug-Ins/LV2 \
       -DVSTPLUGIN_INSTALL_DIR=/Library/Audio/Plug-Ins/VST3 \
+      -DAUPLUGIN_INSTALL_DIR=/Library/Audio/Plug-Ins/Components \
       ..
 make -j$(sysctl -n hw.ncpu)
 # Xcode not currently supported, see https://gitlab.kitware.com/cmake/cmake/issues/18088
