611759a4ecf56317b92de6a7c4ddf774a85c523e
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-05-28 01:38:32 +0200

    Add MinGW dependencies fontconfig and freetype2

--- .travis/install_mingw.sh
+++ .travis/install_mingw.sh
@@ -4,5 +4,5 @@ set -ex
 . .travis/docker_container.sh
 
 buildenv_as_root pacman -Sqyu --noconfirm
-buildenv_as_root pacman -Sq --noconfirm base-devel wget mingw-w64-cmake mingw-w64-gcc mingw-w64-pkg-config mingw-w64-libsndfile mingw-w64-cairo
+buildenv_as_root pacman -Sq --noconfirm base-devel wget mingw-w64-cmake mingw-w64-gcc mingw-w64-pkg-config mingw-w64-libsndfile mingw-w64-cairo mingw-w64-fontconfig mingw-w64-freetype2
 buildenv i686-w64-mingw32-gcc -v && buildenv i686-w64-mingw32-g++ -v && buildenv i686-w64-mingw32-cmake --version
