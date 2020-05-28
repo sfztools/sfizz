9beb6b9d65bcd2f5ddce901701aff8890d9825b4
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-05-28 15:24:38 +0200

    Try fixing homebrew install

--- .travis/install_osx.sh
+++ .travis/install_osx.sh
@@ -5,7 +5,4 @@ set -ex
 sudo ln -s /usr/local /opt/local
 brew update
 brew upgrade cmake
-brew install jack
-brew install dylibbundler
-brew install cairo
-brew install fontconfig
+brew install jack cairo fontconfig dylibbundler
