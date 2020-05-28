f41a71b13ba5f333243a062350fac482b7f3348a
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-05-28 19:01:48 +0200

    Travis scripts update

--- .travis/script_plugins.sh
+++ .travis/script_plugins.sh
@@ -5,6 +5,7 @@ mkdir -p build/${INSTALL_DIR} && cd build
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DSFIZZ_JACK=OFF \
       -DSFIZZ_VST="$ENABLE_VST_PLUGIN" \
+      -DSFIZZ_LV2_UI="$ENABLE_LV2_UI" \
       -DSFIZZ_TESTS=OFF \
       -DSFIZZ_SHARED=OFF \
       -DSFIZZ_STATIC_DEPENDENCIES=ON \
