2bf960df2717812798d630bfed155dd835a5fc63
Paul Ferrand <paul@ferrand.cc>
2020-05-29 14:02:24 +0200

    check span size is a check

--- src/sfizz/MathHelpers.h
+++ src/sfizz/MathHelpers.h
@@ -397,7 +397,7 @@ constexpr bool checkSpanSizes(const absl::Span<T>& span1, Others... others)
     return _checkSpanSizes(span1.size(), others...);
 }
 
-#define CHECK_SPAN_SIZES(...) ASSERT(checkSpanSizes(__VA_ARGS__))
+#define CHECK_SPAN_SIZES(...) CHECK(checkSpanSizes(__VA_ARGS__))
 
 
 class ScopedRoundingMode {
