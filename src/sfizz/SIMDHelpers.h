6e811f92b234f01ccdb7846e75cd231a194bef43
Paul Ferrand <paul@ferrand.cc>
2020-05-30 20:41:25 +0200

    Alignment helpers for possibly any sizes

--- src/sfizz/SIMDHelpers.h
+++ src/sfizz/SIMDHelpers.h
@@ -74,30 +74,30 @@ enum class SIMDOps {
 void setSIMDOpStatus(SIMDOps op, bool status);
 bool getSIMDOpStatus(SIMDOps op);
 
-constexpr uintptr_t ByteAlignmentMask { config::defaultAlignment - 1 };
+constexpr uintptr_t ByteAlignmentMask(unsigned N) { return N - 1; }
 
-template<class T>
+template<unsigned N = config::defaultAlignment, class T>
 T* nextAligned(const T* ptr)
 {
-    return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(ptr) + ByteAlignmentMask & (~ByteAlignmentMask));
+    return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(ptr) + ByteAlignmentMask(N) & (~ByteAlignmentMask(N)));
 }
 
-template<class T>
+template<unsigned N = config::defaultAlignment, class T>
 T* prevAligned(const T* ptr)
 {
-    return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(ptr) & (~ByteAlignmentMask));
+    return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(ptr) & (~ByteAlignmentMask(N)));
 }
 
-template<class T>
+template<unsigned N = config::defaultAlignment, class T>
 bool unaligned(const T* ptr)
 {
-    return (reinterpret_cast<uintptr_t>(ptr) & ByteAlignmentMask )!= 0;
+    return (reinterpret_cast<uintptr_t>(ptr) & ByteAlignmentMask(N) )!= 0;
 }
 
-template<class T, class... Args>
+template<unsigned N = config::defaultAlignment, class T, class... Args>
 bool unaligned(const T* ptr1, Args... rest)
 {
-    return unaligned(ptr1) || unaligned(rest...);
+    return unaligned<N>(ptr1) || unaligned<N>(rest...);
 }
 
 /**
