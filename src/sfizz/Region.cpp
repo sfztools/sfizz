0e2ebe6d467ed6951c3721b4177c5edf9962a8b0
Paul Ferrand <paul@ferrand.cc>
2020-07-01 17:37:44 +0200

    ampeg_XX_oncc can now contain multiple modifiers

--- src/sfizz/Region.cpp
+++ src/sfizz/Region.cpp
@@ -816,25 +816,60 @@ bool sfz::Region::parseOpcode(const Opcode& rawOpcode)
         setValueFromOpcode(opcode, amplitudeEG.vel2sustain, Default::egOnCCPercentRange);
         break;
     case hash("ampeg_attack_oncc&"): // also ampeg_attackcc&
-        setCCPairFromOpcode(opcode, amplitudeEG.ccAttack, Default::egOnCCTimeRange);
+        if (opcode.parameters.back() >= config::numCCs)
+            return false;
+
+        if (auto value = readOpcode(opcode.value, Default::egOnCCTimeRange))
+            amplitudeEG.ccAttack[opcode.parameters.back()] = *value;
+
         break;
     case hash("ampeg_decay_oncc&"): // also ampeg_decaycc&
-        setCCPairFromOpcode(opcode, amplitudeEG.ccDecay, Default::egOnCCTimeRange);
+        if (opcode.parameters.back() >= config::numCCs)
+            return false;
+
+        if (auto value = readOpcode(opcode.value, Default::egOnCCTimeRange))
+            amplitudeEG.ccDecay[opcode.parameters.back()] = *value;
+
         break;
     case hash("ampeg_delay_oncc&"): // also ampeg_delaycc&
-        setCCPairFromOpcode(opcode, amplitudeEG.ccDelay, Default::egOnCCTimeRange);
+        if (opcode.parameters.back() >= config::numCCs)
+            return false;
+
+        if (auto value = readOpcode(opcode.value, Default::egOnCCTimeRange))
+            amplitudeEG.ccDelay[opcode.parameters.back()] = *value;
+
         break;
     case hash("ampeg_hold_oncc&"): // also ampeg_holdcc&
-        setCCPairFromOpcode(opcode, amplitudeEG.ccHold, Default::egOnCCTimeRange);
+        if (opcode.parameters.back() >= config::numCCs)
+            return false;
+
+        if (auto value = readOpcode(opcode.value, Default::egOnCCTimeRange))
+            amplitudeEG.ccHold[opcode.parameters.back()] = *value;
+
         break;
     case hash("ampeg_release_oncc&"): // also ampeg_releasecc&
-        setCCPairFromOpcode(opcode, amplitudeEG.ccRelease, Default::egOnCCTimeRange);
+        if (opcode.parameters.back() >= config::numCCs)
+            return false;
+
+        if (auto value = readOpcode(opcode.value, Default::egOnCCTimeRange))
+            amplitudeEG.ccRelease[opcode.parameters.back()] = *value;
+
         break;
     case hash("ampeg_start_oncc&"): // also ampeg_startcc&
-        setCCPairFromOpcode(opcode, amplitudeEG.ccStart, Default::egOnCCPercentRange);
+        if (opcode.parameters.back() >= config::numCCs)
+            return false;
+
+        if (auto value = readOpcode(opcode.value, Default::egOnCCPercentRange))
+            amplitudeEG.ccStart[opcode.parameters.back()] = *value;
+
         break;
     case hash("ampeg_sustain_oncc&"): // also ampeg_sustaincc&
-        setCCPairFromOpcode(opcode, amplitudeEG.ccSustain, Default::egOnCCPercentRange);
+        if (opcode.parameters.back() >= config::numCCs)
+            return false;
+
+        if (auto value = readOpcode(opcode.value, Default::egOnCCPercentRange))
+            amplitudeEG.ccSustain[opcode.parameters.back()] = *value;
+
         break;
 
     case hash("effect&"):
