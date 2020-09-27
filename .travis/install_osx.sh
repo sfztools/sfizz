0717c2b15df01ef8b58524b3be7a7ff322d09af8
redtide <redtid3@gmail.com>
2020-09-27 19:03:16 +0200

    CI: Use Travis Homebrew addons instead install script

--- .travis/install_osx.sh
+++ .travis/install_osx.sh
@@ -3,9 +3,3 @@
 set -ex
 
 sudo ln -s /usr/local /opt/local
-brew cask uninstall --force java
-brew update
-brew upgrade cmake
-brew install python ||  brew link --overwrite python
-brew install jack
-brew install dylibbundler
