f552f48b514b7711005a6cf184ec03ad965b132e
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-10-07 17:43:01 +0200

    Clang-tidy update

--- scripts/run_clang_tidy.sh
+++ scripts/run_clang_tidy.sh
@@ -31,6 +31,7 @@ clang-tidy \
   vst/SfizzVstEditor.cpp \
   vst/SfizzVstState.cpp \
   -- -Iexternal/abseil-cpp -Iexternal/jsl/include -Isrc/external -Isrc/external/pugixml/src \
+     -Iexternal/st_audiofile/src -Iexternal/st_audiofile/thirdparty/dr_libs \
       -Isrc/sfizz -Isrc -Isrc/external/spline -Isrc/external/cpuid/src \
       -Ivst -Ivst/external/VST_SDK/VST3_SDK -Ivst/external/VST_SDK/VST3_SDK/vstgui4 -Ivst/external/ring_buffer \
       -Ieditor/src \
