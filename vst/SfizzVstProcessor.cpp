cc24203f2d4f4d4bc5771639380d5029c88f4628
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-12-09 17:08:27 +0100

    Log the found file with full path

--- vst/SfizzVstProcessor.cpp
+++ vst/SfizzVstProcessor.cpp
@@ -132,7 +132,7 @@ tresult PLUGIN_API SfizzVstProcessor::setState(IBStream* stream)
         if (!fileScan.locateRealFile(pathOrig, pathFound))
             fprintf(stderr, "[Sfizz] file not found: %s\n", pathOrig.filename().u8string().c_str());
         else {
-            fprintf(stderr, "[Sfizz] file found: %s\n", pathFound.filename().u8string().c_str());
+            fprintf(stderr, "[Sfizz] file found: %s\n", pathFound.u8string().c_str());
             *statePath = pathFound.u8string();
         }
     }
