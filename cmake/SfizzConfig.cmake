a3f93f5d0b41bdbf6a18c763e4d8e743d7fbf05d
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-12-14 14:11:57 +0100

    Reorganize the dependency checking

--- cmake/SfizzConfig.cmake
+++ cmake/SfizzConfig.cmake
@@ -1,6 +1,5 @@
 include(CMakeDependentOption)
 include(CheckCXXCompilerFlag)
-include(CheckLibraryExists)
 include(GNUWarnings)
 
 set(CMAKE_CXX_STANDARD 11 CACHE STRING "C++ standard to be used")
@@ -28,26 +27,6 @@ endif()
 # Do not define macros `min` and `max`
 if(WIN32)
     add_compile_definitions(NOMINMAX)
-endif()
-
-# Find macOS system libraries
-if(APPLE)
-    find_library(APPLE_COREFOUNDATION_LIBRARY "CoreFoundation")
-    find_library(APPLE_FOUNDATION_LIBRARY "Foundation")
-    find_library(APPLE_COCOA_LIBRARY "Cocoa")
-    find_library(APPLE_CARBON_LIBRARY "Carbon")
-    find_library(APPLE_OPENGL_LIBRARY "OpenGL")
-    find_library(APPLE_ACCELERATE_LIBRARY "Accelerate")
-    find_library(APPLE_QUARTZCORE_LIBRARY "QuartzCore")
-    find_library(APPLE_AUDIOTOOLBOX_LIBRARY "AudioToolbox")
-    find_library(APPLE_AUDIOUNIT_LIBRARY "AudioUnit")
-    find_library(APPLE_COREAUDIO_LIBRARY "CoreAudio")
-    find_library(APPLE_COREMIDI_LIBRARY "CoreMIDI")
-    # See https://stackoverflow.com/a/54103956
-    # and https://stackoverflow.com/a/21692023
-    # Apparently this is not needed in Travis CI using addons
-    # but it is in Appveyor instead
-    list(APPEND CMAKE_PREFIX_PATH /usr/local)
 endif()
 
 # The variable CMAKE_SYSTEM_PROCESSOR is incorrect on Visual studio...
@@ -90,48 +69,6 @@ function(sfizz_enable_fast_math NAME)
     endif()
 endfunction()
 
-# The jsl utility library for C++
-add_library(sfizz_jsl INTERFACE)
-add_library(sfizz::jsl ALIAS sfizz_jsl)
-target_include_directories(sfizz_jsl INTERFACE "external/jsl/include")
-
-# The cxxopts library
-add_library(sfizz_cxxopts INTERFACE)
-add_library(sfizz::cxxopts ALIAS sfizz_cxxopts)
-target_include_directories(sfizz_cxxopts INTERFACE "external/cxxopts")
-
-# The sndfile library
-if(SFIZZ_USE_SNDFILE OR SFIZZ_DEMOS OR SFIZZ_BENCHMARKS OR SFIZZ_RENDER)
-    add_library(sfizz_sndfile INTERFACE)
-    add_library(sfizz::sndfile ALIAS sfizz_sndfile)
-    if(SFIZZ_USE_VCPKG OR CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-        find_package(SndFile CONFIG REQUIRED)
-        find_path(SNDFILE_INCLUDE_DIR "sndfile.hh")
-        target_include_directories(sfizz_sndfile INTERFACE "${SNDFILE_INCLUDE_DIR}")
-        target_link_libraries(sfizz_sndfile INTERFACE SndFile::sndfile)
-    else()
-        find_package(PkgConfig REQUIRED)
-        pkg_check_modules(SNDFILE "sndfile" REQUIRED)
-        target_include_directories(sfizz_sndfile INTERFACE ${SNDFILE_INCLUDE_DIRS})
-        if(SFIZZ_STATIC_DEPENDENCIES)
-            target_link_libraries(sfizz_sndfile INTERFACE ${SNDFILE_STATIC_LIBRARIES})
-        else()
-            target_link_libraries(sfizz_sndfile INTERFACE ${SNDFILE_LIBRARIES})
-        endif()
-        link_directories(${SNDFILE_LIBRARY_DIRS})
-    endif()
-endif()
-
-# The st_audiofile library
-if(SFIZZ_USE_SNDFILE)
-    set(ST_AUDIO_FILE_USE_SNDFILE ON CACHE BOOL "" FORCE)
-    set(ST_AUDIO_FILE_EXTERNAL_SNDFILE "sfizz::sndfile" CACHE STRING ""  FORCE)
-else()
-    set(ST_AUDIO_FILE_USE_SNDFILE OFF CACHE BOOL "" FORCE)
-    set(ST_AUDIO_FILE_EXTERNAL_SNDFILE "" CACHE STRING ""  FORCE)
-endif()
-add_subdirectory("external/st_audiofile" EXCLUDE_FROM_ALL)
-
 # If we build with Clang, optionally use libc++. Enabled by default on Apple OS.
 cmake_dependent_option(USE_LIBCPP "Use libc++ with clang" "${APPLE}"
     "CMAKE_CXX_COMPILER_ID MATCHES Clang" OFF)
@@ -140,49 +77,6 @@ if(USE_LIBCPP)
     # Presumably need the above for linking too, maybe other options missing as well
     add_link_options(-stdlib=libc++)   # New command on CMake master, not in 3.12 release
     add_link_options(-lc++abi)   # New command on CMake master, not in 3.12 release
-endif()
-
-add_library(sfizz_pugixml STATIC "src/external/pugixml/src/pugixml.cpp")
-add_library(sfizz::pugixml ALIAS sfizz_pugixml)
-target_include_directories(sfizz_pugixml PUBLIC "src/external/pugixml/src")
-
-add_library(sfizz_spline STATIC "src/external/spline/spline/spline.cpp")
-add_library(sfizz::spline ALIAS sfizz_spline)
-target_include_directories(sfizz_spline PUBLIC "src/external/spline")
-
-add_library(sfizz_tunings STATIC "src/external/tunings/src/Tunings.cpp")
-add_library(sfizz::tunings ALIAS sfizz_tunings)
-target_include_directories(sfizz_tunings PUBLIC "src/external/tunings/include")
-
-add_library(sfizz_hiir INTERFACE)
-add_library(sfizz::hiir ALIAS sfizz_hiir)
-target_include_directories(sfizz_hiir INTERFACE "src/external/hiir")
-
-add_library(sfizz_threadpool INTERFACE)
-add_library(sfizz::threadpool ALIAS sfizz_threadpool)
-target_include_directories(sfizz_threadpool INTERFACE "external/threadpool")
-
-add_library(sfizz_atomic_queue INTERFACE)
-add_library(sfizz::atomic_queue ALIAS sfizz_atomic_queue)
-target_include_directories(sfizz_atomic_queue INTERFACE "external/atomic_queue/include")
-
-add_library(sfizz_filesystem INTERFACE)
-add_library(sfizz::filesystem ALIAS sfizz_filesystem)
-target_include_directories(sfizz_filesystem INTERFACE "external/filesystem/include")
-
-add_library(sfizz_atomic INTERFACE)
-add_library(sfizz::atomic ALIAS sfizz_atomic)
-if(UNIX AND NOT APPLE)
-    file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/check_libatomic")
-    file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/check_libatomic/check_libatomic.c" "int main() { return 0; }")
-    try_compile(SFIZZ_LINK_LIBATOMIC "${CMAKE_CURRENT_BINARY_DIR}/check_libatomic"
-        SOURCES "${CMAKE_CURRENT_BINARY_DIR}/check_libatomic/check_libatomic.c"
-        LINK_LIBRARIES "atomic")
-    if(SFIZZ_LINK_LIBATOMIC)
-        target_link_libraries(sfizz_atomic INTERFACE "atomic")
-    endif()
-else()
-    set(SFIZZ_LINK_LIBATOMIC FALSE)
 endif()
 
 # Don't show build information when building a different project
@@ -204,7 +98,6 @@ Build tests:                   ${SFIZZ_TESTS}
 Use sndfile:                   ${SFIZZ_USE_SNDFILE}
 Use vcpkg:                     ${SFIZZ_USE_VCPKG}
 Statically link dependencies:  ${SFIZZ_STATIC_DEPENDENCIES}
-Link libatomic:                ${SFIZZ_LINK_LIBATOMIC}
 Use clang libc++:              ${USE_LIBCPP}
 Release asserts:               ${SFIZZ_RELEASE_ASSERTS}
 
@@ -217,5 +110,3 @@ Compiler CXX min size flags:   ${CMAKE_CXX_FLAGS_MINSIZEREL}
 ")
     endif()
 endfunction()
-
-find_package(Threads REQUIRED)
