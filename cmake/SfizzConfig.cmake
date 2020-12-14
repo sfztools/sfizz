8b56f89688ab7f84c7fb92bf4509d8a0f713831d
Jean Pierre Cimalando <jp-dev@inbox.ru>
2020-12-14 07:49:02 +0100

    Update cmake files formatting

--- cmake/SfizzConfig.cmake
+++ cmake/SfizzConfig.cmake
@@ -6,26 +6,26 @@ set(CMAKE_CXX_STANDARD 11 CACHE STRING "C++ standard to be used")
 set(CMAKE_C_STANDARD 99 CACHE STRING "C standard to be used")
 
 # Export the compile_commands.json file
-set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 # Only install what's explicitely said
-set (CMAKE_SKIP_INSTALL_ALL_DEPENDENCY true)
-set (CMAKE_POSITION_INDEPENDENT_CODE ON)
-set (CMAKE_CXX_VISIBILITY_PRESET hidden)
-set (CMAKE_VISIBILITY_INLINES_HIDDEN ON)
+set(CMAKE_SKIP_INSTALL_ALL_DEPENDENCY true)
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+set(CMAKE_CXX_VISIBILITY_PRESET hidden)
+set(CMAKE_VISIBILITY_INLINES_HIDDEN ON)
 
 # Set Windows compatibility level to 7
-if (WIN32)
+if(WIN32)
     add_compile_definitions(_WIN32_WINNT=0x601)
 endif()
 
 # Set macOS compatibility level
-if (APPLE)
+if(APPLE)
     set(CMAKE_OSX_DEPLOYMENT_TARGET "10.9")
 endif()
 
 # Do not define macros `min` and `max`
-if (WIN32)
+if(WIN32)
     add_compile_definitions(NOMINMAX)
 endif()
 
@@ -46,13 +46,13 @@ if(APPLE)
     # and https://stackoverflow.com/a/21692023
     # Apparently this is not needed in Travis CI using addons
     # but it is in Appveyor instead
-    list (APPEND CMAKE_PREFIX_PATH /usr/local)
+    list(APPEND CMAKE_PREFIX_PATH /usr/local)
 endif()
 
 # The variable CMAKE_SYSTEM_PROCESSOR is incorrect on Visual studio...
 # see https://gitlab.kitware.com/cmake/cmake/issues/15170
 
-if (NOT SFIZZ_SYSTEM_PROCESSOR)
+if(NOT SFIZZ_SYSTEM_PROCESSOR)
     if(MSVC)
         set(SFIZZ_SYSTEM_PROCESSOR "${MSVC_CXX_ARCHITECTURE_ID}")
     else()
@@ -61,13 +61,13 @@ if (NOT SFIZZ_SYSTEM_PROCESSOR)
 endif()
 
 # Add required flags for the builds
-if (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
     gw_warn(-Wall -Wextra -Wno-multichar -Werror=return-type)
-    if (SFIZZ_SYSTEM_PROCESSOR MATCHES "^(i.86|x86_64)$")
+    if(SFIZZ_SYSTEM_PROCESSOR MATCHES "^(i.86|x86_64)$")
         add_compile_options(-msse2)
     elseif(SFIZZ_SYSTEM_PROCESSOR MATCHES "^(arm.*)$")
         add_compile_options(-mfpu=neon)
-        if (NOT ANDROID)
+        if(NOT ANDROID)
             add_compile_options(-mfloat-abi=hard)
         endif()
     endif()
@@ -75,14 +75,14 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
     if(SFIZZ_HAVE_FALIGNED_NEW)
         add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-faligned-new>)
     endif()
-elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
     set(CMAKE_CXX_STANDARD 17)
     add_compile_options(/Zc:__cplusplus)
     set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
 endif()
 
 function(sfizz_enable_fast_math NAME)
-    if (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+    if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
         target_compile_options("${NAME}" PRIVATE "-ffast-math")
     elseif(MSVC)
         target_compile_options("${NAME}" PRIVATE "/fp:fast")
@@ -98,9 +98,9 @@ add_library(sfizz-cxxopts INTERFACE)
 target_include_directories(sfizz-cxxopts INTERFACE "external/cxxopts")
 
 # The sndfile library
-if (SFIZZ_USE_SNDFILE OR SFIZZ_DEMOS OR SFIZZ_BENCHMARKS OR SFIZZ_RENDER)
+if(SFIZZ_USE_SNDFILE OR SFIZZ_DEMOS OR SFIZZ_BENCHMARKS OR SFIZZ_RENDER)
     add_library(sfizz-sndfile INTERFACE)
-    if (SFIZZ_USE_VCPKG OR CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+    if(SFIZZ_USE_VCPKG OR CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
         find_package(SndFile CONFIG REQUIRED)
         find_path(SNDFILE_INCLUDE_DIR "sndfile.hh")
         target_include_directories(sfizz-sndfile INTERFACE "${SNDFILE_INCLUDE_DIR}")
@@ -109,7 +109,7 @@ if (SFIZZ_USE_SNDFILE OR SFIZZ_DEMOS OR SFIZZ_BENCHMARKS OR SFIZZ_RENDER)
         find_package(PkgConfig REQUIRED)
         pkg_check_modules(SNDFILE "sndfile" REQUIRED)
         target_include_directories(sfizz-sndfile INTERFACE ${SNDFILE_INCLUDE_DIRS})
-        if (SFIZZ_STATIC_DEPENDENCIES)
+        if(SFIZZ_STATIC_DEPENDENCIES)
             target_link_libraries(sfizz-sndfile INTERFACE ${SNDFILE_STATIC_LIBRARIES})
         else()
             target_link_libraries(sfizz-sndfile INTERFACE ${SNDFILE_LIBRARIES})
@@ -119,7 +119,7 @@ if (SFIZZ_USE_SNDFILE OR SFIZZ_DEMOS OR SFIZZ_BENCHMARKS OR SFIZZ_RENDER)
 endif()
 
 # The st_audiofile library
-if (SFIZZ_USE_SNDFILE)
+if(SFIZZ_USE_SNDFILE)
     set(ST_AUDIO_FILE_USE_SNDFILE ON CACHE BOOL "" FORCE)
     set(ST_AUDIO_FILE_EXTERNAL_SNDFILE "sfizz-sndfile" CACHE STRING ""  FORCE)
 else()
@@ -131,7 +131,7 @@ add_subdirectory("external/st_audiofile" EXCLUDE_FROM_ALL)
 # If we build with Clang, optionally use libc++. Enabled by default on Apple OS.
 cmake_dependent_option(USE_LIBCPP "Use libc++ with clang" "${APPLE}"
     "CMAKE_CXX_COMPILER_ID MATCHES Clang" OFF)
-if (USE_LIBCPP)
+if(USE_LIBCPP)
     add_compile_options(-stdlib=libc++)
     # Presumably need the above for linking too, maybe other options missing as well
     add_link_options(-stdlib=libc++)   # New command on CMake master, not in 3.12 release
@@ -147,25 +147,25 @@ target_include_directories(sfizz-spline PUBLIC "src/external/spline")
 add_library(sfizz-tunings STATIC "src/external/tunings/src/Tunings.cpp")
 target_include_directories(sfizz-tunings PUBLIC "src/external/tunings/include")
 
-include (CheckLibraryExists)
-add_library (sfizz-atomic INTERFACE)
-if (UNIX AND NOT APPLE)
+include(CheckLibraryExists)
+add_library(sfizz-atomic INTERFACE)
+if(UNIX AND NOT APPLE)
     file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/check_libatomic")
     file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/check_libatomic/check_libatomic.c" "int main() { return 0; }")
     try_compile(SFIZZ_LINK_LIBATOMIC "${CMAKE_CURRENT_BINARY_DIR}/check_libatomic"
         SOURCES "${CMAKE_CURRENT_BINARY_DIR}/check_libatomic/check_libatomic.c"
         LINK_LIBRARIES "atomic")
-    if (SFIZZ_LINK_LIBATOMIC)
-        target_link_libraries (sfizz-atomic INTERFACE "atomic")
+    if(SFIZZ_LINK_LIBATOMIC)
+        target_link_libraries(sfizz-atomic INTERFACE "atomic")
     endif()
 else()
     set(SFIZZ_LINK_LIBATOMIC FALSE)
 endif()
 
 # Don't show build information when building a different project
-function (show_build_info_if_needed)
-    if (CMAKE_PROJECT_NAME STREQUAL "sfizz")
-        message (STATUS "
+function(show_build_info_if_needed)
+    if(CMAKE_PROJECT_NAME STREQUAL "sfizz")
+        message(STATUS "
 Project name:                  ${PROJECT_NAME}
 Build type:                    ${CMAKE_BUILD_TYPE}
 Build processor:               ${SFIZZ_SYSTEM_PROCESSOR}
@@ -195,4 +195,4 @@ Compiler CXX min size flags:   ${CMAKE_CXX_FLAGS_MINSIZEREL}
     endif()
 endfunction()
 
-find_package (Threads REQUIRED)
+find_package(Threads REQUIRED)
