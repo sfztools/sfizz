9e362a5507961a42b9f06902df677fe10ff8d8bd
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-03-07 18:08:27 +0100

    Ensure to not set vst libs to a lower standard

--- plugins/vst/cmake/Vst3.cmake
+++ plugins/vst/cmake/Vst3.cmake
@@ -36,7 +36,9 @@ function(plugin_add_vst3sdk NAME)
             "${VST3SDK_BASEDIR}/public.sdk/source/common/threadchecker_win32.cpp"
             "${VST3SDK_BASEDIR}/public.sdk/source/vst/vstgui_win32_bundle_support.cpp"
             "${VST3SDK_BASEDIR}/public.sdk/source/main/dllmain.cpp")
-        set_property(TARGET "${NAME}" PROPERTY CXX_STANDARD 14)
+        if(NOT CMAKE_CXX_STANDARD OR CMAKE_CXX_STANDARD LESS 14)
+            set_property(TARGET "${NAME}" PROPERTY CXX_STANDARD 14)
+        endif()
     elseif(APPLE)
         target_sources("${NAME}" PRIVATE
             "${VST3SDK_BASEDIR}/public.sdk/source/main/macmain.cpp")
