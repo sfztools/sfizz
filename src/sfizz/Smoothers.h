75ca9b7d82155f63085afcfbb07a94ea49516df7
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-03-13 12:37:41 +0100

    Linear smoother

--- src/sfizz/Smoothers.h
+++ src/sfizz/Smoothers.h
@@ -14,9 +14,9 @@ namespace sfz {
  * @brief Wrapper class for a one pole filter smoother
  *
  */
-class Smoother {
+class OnePoleSmoother {
 public:
-    Smoother();
+    OnePoleSmoother();
     /**
      * @brief Set the filter cutoff based on the sfz smoothing value
      * and the sample rate.
@@ -48,5 +48,52 @@ private:
     bool smoothing { false };
     OnePoleFilter<float> filter {};
 };
+
+/**
+ * @brief Linear smoother
+ *
+ */
+class LinearSmoother {
+public:
+    LinearSmoother();
+    /**
+     * @brief Set the filter cutoff based on the sfz smoothing value
+     * and the sample rate.
+     *
+     * @param smoothValue
+     * @param sampleRate
+     */
+    void setSmoothing(uint8_t smoothValue, float sampleRate);
+    /**
+     * @brief Reset the filter state to a given value
+     *
+     * @param value
+     */
+    void reset(float value = 0.0f);
+    /**
+     * @brief Process a span of data. Input and output can refer to the same
+     * memory.
+     *
+     * @param input
+     * @param output
+     * @param canShortcut whether we can have a fast path if the filter is within
+     *                    a reasonable range around the first value of the input
+     *                    span.
+     */
+    void process(absl::Span<const float> input, absl::Span<float> output, bool canShortcut = false);
+
+    float current() const { return current_; }
+private:
+    float current_ = 0.0;
+    float target_ = 0.0;
+    float step_ = 0.0;
+    int32_t framesToTarget_ = 0;
+    int32_t smoothFrames_ = 0;
+};
+
+/**
+ * @brief Default smoother
+ */
+using Smoother = LinearSmoother;
 
 }
