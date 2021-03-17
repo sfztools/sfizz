e77a227cbf72e7e8422bb7e6a5abfc8e00b8f89f
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-03-18 00:42:04 +0100

    Connect unit tests with CTest

--- tests/MainT.cpp
+++ tests/MainT.cpp
@@ -3,8 +3,28 @@
 #define CATCH_CONFIG_RUNNER
 #include "catch2/catch.hpp"
 
+#include <ghc/fs_std.hpp>
+
+static bool moveToTestsDirectory(const fs::path& searchedPath);
+
 int main(int argc, char* argv[])
 {
+    if (!moveToTestsDirectory(fs::u8path("tests/TestFiles"))) {
+        std::cerr << "Failed to locate test files\n";
+        return 1;
+    }
     int result = Catch::Session().run(argc, argv);
     return result;
+}
+
+static bool moveToTestsDirectory(const fs::path& searchedPath)
+{
+    while (!fs::exists(searchedPath)) {
+        fs::path cwdPath = fs::current_path();
+        fs::path newPath = cwdPath.parent_path();
+        if (cwdPath == newPath)
+            return false;
+        fs::current_path(newPath);
+    }
+    return true;
 }
