ce1f11abec42871809ce1b8e0ce289531e89f9a2
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-03-23 17:44:21 +0100

    Remove MSVC from Appveyor

--- scripts/appveyor/install.cmd
+++
@@ -1,9 +0,0 @@
-choco install -y innosetup
-
-REM Uncomment the next 4 lines to force vcpkg update
-REM cd c:\tools\vcpkg\
-REM git pull
-REM .\bootstrap-vcpkg.bat
-REM cd %APPVEYOR_BUILD_FOLDER%
-
-REM vcpkg install libsndfile:%VCPKG_TRIPLET%
