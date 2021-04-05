cb85ca39708b7d12bd9f38bb55941d3f2acfc4cd
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-04-06 00:33:04 +0200

    Track current CC values after restore

--- plugins/lv2/sfizz.cpp
+++ plugins/lv2/sfizz.cpp
@@ -1355,8 +1355,11 @@ restore(LV2_Handle instance,
     for (unsigned cc = 0; cc < sfz::config::numCCs; ++cc) {
         absl::optional<float> value = cc_values[cc];
         if (value) {
+            // Mark CCs for automation with state values
             self->ccauto[cc] = *value;
             self->have_ccauto = true;
+            // Update the current CCs
+            self->cc_current[cc] = *value;
         }
     }
 
