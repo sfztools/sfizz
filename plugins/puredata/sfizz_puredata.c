a9f119d63b77a92986cd9336c52c04ce747826c9
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-04-30 12:03:13 +0200

    Update pd external and help with new features

--- plugins/puredata/sfizz_puredata.c
+++ plugins/puredata/sfizz_puredata.c
@@ -16,12 +16,30 @@ static t_class* cls_sfizz_tilde;
 typedef struct _sfizz_tilde {
     t_object obj;
     t_outlet* outputs[2];
+    t_inlet* input_cc;
+    t_inlet* input_bend;
+    t_inlet* input_touch;
+    t_inlet* input_polytouch;
     sfizz_synth_t* synth;
     int midi[3];
     int midinum;
     t_symbol* dir;
     char* filepath;
 } t_sfizz_tilde;
+
+static t_float clamp01(t_float x)
+{
+    x = (x > 0) ? x : 0;
+    x = (x < 1) ? x : 1;
+    return x;
+}
+
+static t_float clampB1(t_float x)
+{
+    x = (x > -1) ? x : -1;
+    x = (x < 1) ? x : 1;
+    return x;
+}
 
 static void sfizz_tilde_set_file(t_sfizz_tilde* self, const char* file)
 {
@@ -69,6 +87,11 @@ static void* sfizz_tilde_new(t_symbol* sym, int argc, t_atom argv[])
     self->outputs[0] = outlet_new(&self->obj, &s_signal);
     self->outputs[1] = outlet_new(&self->obj, &s_signal);
 
+    self->input_cc = inlet_new(&self->obj, &self->obj.ob_pd, &s_float, gensym("cc"));
+    self->input_bend = inlet_new(&self->obj, &self->obj.ob_pd, &s_float, gensym("bend"));
+    self->input_touch = inlet_new(&self->obj, &self->obj.ob_pd, &s_float, gensym("touch"));
+    self->input_polytouch = inlet_new(&self->obj, &self->obj.ob_pd, &s_float, gensym("polytouch"));
+
     sfizz_synth_t* synth = sfizz_create_synth();
     self->synth = synth;
 
@@ -94,6 +117,14 @@ static void sfizz_tilde_free(t_sfizz_tilde* self)
         outlet_free(self->outputs[0]);
     if (self->outputs[1])
         outlet_free(self->outputs[1]);
+    if (self->input_cc)
+        inlet_free(self->input_cc);
+    if (self->input_bend)
+        inlet_free(self->input_bend);
+    if (self->input_touch)
+        inlet_free(self->input_touch);
+    if (self->input_polytouch)
+        inlet_free(self->input_polytouch);
 }
 
 static t_int* sfizz_tilde_perform(t_int* w)
@@ -118,6 +149,22 @@ static void sfizz_tilde_dsp(t_sfizz_tilde* self, t_signal** sp)
     dsp_add(
         &sfizz_tilde_perform, 4, (t_int)self,
         (t_int)sp[0]->s_vec, (t_int)sp[1]->s_vec, (t_int)sp[0]->s_n);
+}
+
+static void sfizz_tilde_list(t_sfizz_tilde* self, t_symbol* sym, int argc, t_atom* argv)
+{
+    (void)sym;
+
+    if (argc == 2 && argv[0].a_type == A_FLOAT && argv[1].a_type == A_FLOAT) {
+        int key = (int)argv[0].a_w.w_float;
+        if (key < 0 || key > 127)
+            return;
+        t_float vel = clamp01(argv[1].a_w.w_float / 127);
+        if (vel > 0)
+            sfizz_send_hd_note_on(self->synth, 0, key, vel);
+        else
+            sfizz_send_hd_note_off(self->synth, 0, key, 0);
+    }
 }
 
 static void sfizz_tilde_midiin(t_sfizz_tilde* self, t_float f)
@@ -187,17 +234,55 @@ static void sfizz_tilde_reload(t_sfizz_tilde* self, t_float value)
     sfizz_tilde_do_load(self);
 }
 
-static void sfizz_tilde_hdcc(t_sfizz_tilde* self, t_float cc, t_float value)
+static void sfizz_tilde_hdcc(t_sfizz_tilde* self, t_float f1, t_float f2)
 {
-    sfizz_automate_hdcc(self->synth, 0, (int)cc, value);
+    int cc = (int)f1;
+    if (cc < 0 || cc > 127)
+        return;
+    sfizz_automate_hdcc(self->synth, 0, (int)cc, clamp01(f2));
 }
 
-static void sfizz_tilde_voices(t_sfizz_tilde* self, t_float value)
+static void sfizz_tilde_cc(t_sfizz_tilde* self, t_float f1, t_float f2)
 {
-    int numvoices = (int)value;
-    if (numvoices < 1)
-        numvoices = 1;
+    sfizz_tilde_hdcc(self, f1, f2 / 127);
+}
 
+static void sfizz_tilde_hdbend(t_sfizz_tilde* self, t_float f1)
+{
+    sfizz_send_hd_pitch_wheel(self->synth, 0, clampB1(f1));
+}
+
+static void sfizz_tilde_bend(t_sfizz_tilde* self, t_float f1)
+{
+    return sfizz_tilde_hdbend(self, f1 / 8191);
+}
+
+static void sfizz_tilde_hdtouch(t_sfizz_tilde* self, t_float f1)
+{
+    sfizz_send_hd_channel_aftertouch(self->synth, 0, clamp01(f1));
+}
+
+static void sfizz_tilde_touch(t_sfizz_tilde* self, t_float f1)
+{
+    sfizz_tilde_hdtouch(self, f1 / 127);
+}
+
+static void sfizz_tilde_hdpolytouch(t_sfizz_tilde* self, t_float key, t_float f2)
+{
+    if (key < 0 || key > 127)
+        return;
+    sfizz_send_hd_poly_aftertouch(self->synth, 0, (int)key, clamp01(f2));
+}
+
+static void sfizz_tilde_polytouch(t_sfizz_tilde* self, t_float f1, t_float f2)
+{
+    sfizz_tilde_hdpolytouch(self, f1, f2 / 127);
+}
+
+static void sfizz_tilde_voices(t_sfizz_tilde* self, t_float f1)
+{
+    int numvoices = (int)f1;
+    numvoices = (numvoices < 1) ? 1 : numvoices;
     sfizz_set_num_voices(self->synth, numvoices);
 }
 
@@ -218,6 +303,8 @@ void sfizz_setup()
         CLASS_DEFAULT, A_GIMME, A_NULL);
     class_addmethod(
         cls_sfizz_tilde, (t_method)&sfizz_tilde_dsp, gensym("dsp"), A_CANT, A_NULL);
+    class_addlist(
+        cls_sfizz_tilde, (t_method)&sfizz_tilde_list);
     class_addmethod(
         cls_sfizz_tilde, (t_method)&sfizz_tilde_midiin, &s_float, A_FLOAT, A_NULL);
     class_addmethod(
@@ -225,7 +312,21 @@ void sfizz_setup()
     class_addmethod(
         cls_sfizz_tilde, (t_method)&sfizz_tilde_reload, gensym("reload"), A_DEFFLOAT, A_NULL);
     class_addmethod(
+        cls_sfizz_tilde, (t_method)&sfizz_tilde_cc, gensym("cc"), A_FLOAT, A_FLOAT, A_NULL);
+    class_addmethod(
         cls_sfizz_tilde, (t_method)&sfizz_tilde_hdcc, gensym("hdcc"), A_FLOAT, A_FLOAT, A_NULL);
+    class_addmethod(
+        cls_sfizz_tilde, (t_method)&sfizz_tilde_bend, gensym("bend"), A_FLOAT, A_NULL);
+    class_addmethod(
+        cls_sfizz_tilde, (t_method)&sfizz_tilde_hdbend, gensym("hdbend"), A_FLOAT, A_NULL);
+    class_addmethod(
+        cls_sfizz_tilde, (t_method)&sfizz_tilde_touch, gensym("touch"), A_FLOAT, A_NULL);
+    class_addmethod(
+        cls_sfizz_tilde, (t_method)&sfizz_tilde_hdtouch, gensym("hdtouch"), A_FLOAT, A_NULL);
+    class_addmethod(
+        cls_sfizz_tilde, (t_method)&sfizz_tilde_polytouch, gensym("polytouch"), A_FLOAT, A_FLOAT, A_NULL);
+    class_addmethod(
+        cls_sfizz_tilde, (t_method)&sfizz_tilde_hdpolytouch, gensym("hdpolytouch"), A_FLOAT, A_FLOAT, A_NULL);
     class_addmethod(
         cls_sfizz_tilde, (t_method)&sfizz_tilde_voices, gensym("voices"), A_FLOAT, A_NULL);
 }
