f304c0b9889535a2207e42833a00a4b33253ee7c
Praash <praashie@gmail.com>
2021-06-07 14:51:29 +0300

    Use group cuts with CC triggered (pedal) samples

--- src/sfizz/Voice.cpp
+++ src/sfizz/Voice.cpp
@@ -1648,7 +1648,9 @@ bool Voice::checkOffGroup(const Region* other, int delay, int noteNumber) noexce
     if (impl.released())
         return false;
 
-    if (impl.triggerEvent_.type == TriggerEventType::NoteOn
+    if (
+        (impl.triggerEvent_.type == TriggerEventType::NoteOn
+            ||  impl.triggerEvent_.type == TriggerEventType::CC)
         && region->offBy && *region->offBy == other->group
         && (region->group != other->group || noteNumber != impl.triggerEvent_.number)) {
         off(delay);
